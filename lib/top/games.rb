fbc4f14dc2d16b46048e91503cfd61949f351e18
Taishawn King <taishawnking1@yahoo.com>
2020-02-18 05:46:29 +0000

    first commit

---
+++ lib/top/games.rb
@@ -0,0 +1,8 @@
+require "top/games/version"
+
+module Top
+  module Games
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/top/games/version.rb
@@ -0,0 +1,5 @@
+module Top
+  module Games
+    VERSION = "0.1.0"
+  end
+end
